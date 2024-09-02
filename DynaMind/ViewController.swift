021adf9623274640d761a2e9f7fb5f88073c777a
Aline do Amaral <aline.amaral@teamsoft.com.br>
2024-09-02 13:17:13 -0300

    Initial Commit

---
+++ DynaMind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynaMind
+//
+//  Created by Aline Amaral on 02/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
